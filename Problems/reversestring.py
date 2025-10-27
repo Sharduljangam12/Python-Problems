6082a9390c776e2a7269bf0c39190bfaf4138ba7
Shardul Jangam <152073842+Sharduljangam12@users.noreply.github.com>
2025-10-27 21:12:40 +0530

    first commit

---
+++ Problems/reversestring.py
@@ -0,0 +1,7 @@
+#This is the 1st problem in the 100 Problem solving Challenge before 31st Dec 2025
+#Problem: Write a function that takes a string as input and returns the string reversed.
+
+x = ("Wolf")
+print(x)
+y = x[::-1]
+print(y)
