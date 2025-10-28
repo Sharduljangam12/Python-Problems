2e9be4161756db1df8438b24a74c14834ec9dd7c
Shardul Jangam <152073842+Sharduljangam12@users.noreply.github.com>
2025-10-28 20:57:52 +0530

    3rd problem

---
+++ Problems/evenodd.py
@@ -0,0 +1,8 @@
+#This is Day2 2nd problem in the 100 problem solving challenge before 31st dec 2025
+# Problem: enter a number and this program will tell it's even or odd
+
+x = 4
+if x/2 == 0:
+    print("the number is odd")
+else:
+    print("the number is evem")
