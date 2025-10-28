efcc370bc47aae23aaa41f1975321f19b1aaac27
Shardul Jangam <152073842+Sharduljangam12@users.noreply.github.com>
2025-10-28 10:41:10 +0530

    corrected problem statement

--- Problems/reversestring.py
+++ Problems/reversestring.py
@@ -1,5 +1,5 @@
 #This is the 1st problem in the 100 Problem solving Challenge before 31st Dec 2025
-#Problem: Write a function that takes a string as input and returns the string reversed.
+#Problem: Write a function that takes a string and returns the string reversed.
 
 x = ("Wolf")
 print(x)
