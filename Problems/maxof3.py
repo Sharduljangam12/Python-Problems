96445f9962412755bb2219c5b897ad84f717d8ea
Shardul Jangam <152073842+Sharduljangam12@users.noreply.github.com>
2025-10-28 10:58:46 +0530

    2nd program

---
+++ Problems/maxof3.py
@@ -0,0 +1,12 @@
+#This is Day2 in the 100 problem solving challenge before 31st dec 2025
+#Problem : Find the maximum between 3 numbers 
+x = 100
+y = 2900
+z = 800
+
+if x > y >z:
+    print(x)
+elif y > z > x:
+    print(y)
+else:
+    print(z)
