e893bcac27e2bc3a41390d7206e2efe750be874f
Shardul Jangam <152073842+Sharduljangam12@users.noreply.github.com>
2026-01-02 22:45:34 +0530

    the 2nd day of 2026

---
+++ Problems/maxof2.py
@@ -0,0 +1,9 @@
+#In this program we will find which number is bigger and which is smaller 
+
+x = int(input())
+y = int(input())
+
+if x > y:
+    print("first number is the Bigger ")
+else:
+    print("Second number is Bigger ")
